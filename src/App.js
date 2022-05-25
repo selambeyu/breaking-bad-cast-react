191b854ef4b39e9c9d11962d9750d0387c3ada22
melkam_beyene <melkam3392beyu@gmail.com>
2022-05-25 08:23:44 +0300

    add styles and app.js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-function App() {
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from './components/ui/Search'
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query,setQuery]=useState('')
+
+  useEffect(() => {
+    const fetchitem = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
+      console.log("resuslt", result.data);
+      setItems(result.data);
+      setIsLoading(false)
+    };
+    fetchitem();
+   
+  },[query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-      
-        <p>
-         Hi Melkam!
-        </p>
-        
-      </header>
+    <div className="container">
+      <Header />
+      <Search getQuery={(q)=>setQuery(q)}/>
+      <CharacterGrid isLoading={isLoading} items={items}/>
     </div>
   );
-}
+};
 
 export default App;
